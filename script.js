969f873b3b506ba5614e948c7333b94ccccd69f9
Arpita Desai <patelarpita29@gmail.com>
2020-04-04 22:21:22 -0700

    First Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+
+//This snippet is for displaying current Day.
+var presentDay = $("#currentDay");
+var dateDay = new Date();
+var displayDay = dateDay.toString().slice(0,15);
+presentDay.text(`${displayDay}`);
+//console.log(displayDay);
+
+
+
