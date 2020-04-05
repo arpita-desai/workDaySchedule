d0667530529708e1062118f6f91e43815afdf61e
Arpita Desai <patelarpita29@gmail.com>
2020-04-05 11:33:39 -0700

    on sunday

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 
 //This snippet is for displaying current Day.
 var presentDay = $("#currentDay");
+
 var dateDay = new Date();
 var displayDay = dateDay.toString().slice(0,15);
 presentDay.text(`${displayDay}`);
 //console.log(displayDay);
 
-var textArea = $("#textArea");
+var textArea = $(".textArea");
 var saveBtn = $(".saveBtn");
+ 
 
 
 
